f742aa3a856d217551ed90ead49d179affb21fd1
13x54n <laxman.rai.07.26@gmail.com>
2023-06-24 01:10:17 -0400

    feat:added dynamic balance of ether and adress but for sending transaction, need to store privateKey separately.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <ToastContainer />
+  </React.StrictMode>
+);
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { wallet } from "./WelcomePage";
 import { ethers } from "ethers";
+import { toast } from "react-toastify";
+import { wallet } from "./WelcomePage";
 
 const provider = new ethers.JsonRpcProvider(
   import.meta.env.VITE_APP_INFURA_ENDPOINT
@@ -24,6 +25,30 @@ export default function Dashboard() {
     });
   }, []);
 
+  const [receiverAddress, setReceiverAddress] = React.useState("");
+  const [receivingAmount, setReceivingAmount] = React.useState("");
+
+  const handleTransactionSend = () => {
+    console.log(wallet.privateKey)
+    const signer = new ethers.Wallet(wallet.privateKey, provider);
+
+    let tx = {
+      to: receiverAddress,
+      // Convert currency unit from ether to wei
+      value: ethers.parseEther(receivingAmount),
+    };
+
+    signer
+      .sendTransaction(tx)
+      .then(() => toast("Transaction Submitted!"))
+      .then((txObj) => {
+        console.log("txHash", txObj.hash);
+        toast("Transaction Completed!");
+        // => 0x9c172314a693b94853b49dc057cf1cb8e529f29ce0272f451eea8f5741aa9b58
+        // A transaction result can be checked in a etherscan with a transaction hash which can be obtained here.
+      });
+  };
+
   return (
     <div>
       <h1>Hey, 13x54r Wallet User! 👋</h1>
@@ -43,8 +68,19 @@ export default function Dashboard() {
         <>
           <p>Public Address: {wallet?.address}</p>
           <p>Balance: {balance} ether</p>
-          <input type="text" placeholder="Enter Friend's Address..."/><input type="text" placeholder="Enter Value..."/>
-          <button>Send</button>
+          <input
+            type="text"
+            placeholder="Enter Friend's Address..."
+            value={receiverAddress}
+            onChange={(e) => setReceiverAddress(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Enter Value..."
+            value={receivingAmount}
+            onChange={(e) => setReceivingAmount(e.target.value)}
+          />
+          <button onClick={() => handleTransactionSend()}>Send</button>
           <hr />
           <h3>Transaction History</h3>
           <p>⚠️ For now, history is in progress!</p>
