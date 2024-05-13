0281fcf53cf682a86360134a50bdb05a44bd6c47
Sangya Sherpa <135448616+13x54n@users.noreply.github.com>
2024-05-13 18:04:01 +0000

    Updated Codebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <ToastContainer />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
